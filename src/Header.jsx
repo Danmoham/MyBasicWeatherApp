f719d986cbfd234bca8082d0aa3f7b2dcf9226c0
Danmoham <dmoham35@gmail.com>
2023-10-15 19:27:48 +0100

    done

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
 export const Header = () =>{
 
-return (<div id="headDiv"><h1>WorldWide Temperature and Time Checker for cities and towns!</h1></div>)
+return (<div id="headDiv"><h1>WorldWide Temperature and Time Checker!</h1></div>)
 }
